776bbaa0009e1afdce45da0a2dfd7770922b9e7c
Leyna May <leyna.may@durham.ac.uk>
2018-07-03 10:38:11 +0100

    Using numpy

--- Main.py
+++ Main.py
@@ -1,14 +1,16 @@
+import numpy as np
+
 def adjmat(a):
     length = 5 + len(a)
-    m = [[0 for i in range (length)] for i in range (length)]
-    m[0:4][0:4] = one_ball(a[0])
+    m = np.zeros((length,length))
+    m[0:5,0:5] = one_ball(a[0])
     return m
 
 
 
 
 def one_ball(a1):
-    m1 = [[1 for i in range(5)] for i in range(5)]
+    m1 = np.ones((5,5))
     i = (1,1,1,2,2,3)
     j = (2,3,4,3,4,4)
     for n in range(6):
@@ -18,5 +20,6 @@ def one_ball(a1):
     return m1
 
 print adjmat(((1,1,1,0,0,0),(0)))
+
 
 
