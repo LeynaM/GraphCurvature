1677ac1d368ef537ae72b3e63a2431db38458a57
Leyna May <leyna.may@durham.ac.uk>
2018-07-03 12:07:18 +0100

    added twoball2

--- Main.py
+++ Main.py
@@ -24,12 +24,25 @@ def one_ball(a1):
 
 def two_ball(a2, m):
     for i in range(len(a2)):
-        for j in range(len(a2[i])):
-            m[i+5][a2[i][j]] =  1
-            m[a2[i][j]][i+5] =  1
-            m[i+5][i+5] = 1
+        m[i+5][i+5] = 1
+        if type(a2[i]) is int:
+            m[i+5][a2[i]] = 1
+            m[a2[i]][i+5] = 1
+        else:
+            for j in range(len(a2[i])):
+                m[i+5][a2[i][j]] =  1
+                m[a2[i][j]][i+5] =  1
+            
                 
 
-print adjmat(((1,1,1,0,0,0),(2,3),(3,4)))
+print adjmat(((1,1,1,0,0,0),(2,3),(3,4),(1)))
 
-
+def two_ball2(a2, m):
+    for i in range(len(a2)):
+        m[i+5][i+5] = 1
+        for j in range(4):
+            if a2[i][j] == 1:
+                m[i+5][j+1] = 1
+                m[j+1][i+5] = 1
+             
+print adjmat(((1,1,1,0,0,0),(0,1,1,0),(0,0,1,1),(1,0,0,0)))
